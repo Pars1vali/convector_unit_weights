afe0ddedb54e7afb0db196c61911c06463520512
Chumak Vladimir <parsival427@gmail.com>
2023-12-02 19:53:42 +0300

    Upload project!

---
+++ covector.py
@@ -0,0 +1,53 @@
+import pandas as pd
+def convert_weight_kg(weight_in_kg):
+    # Конвертация в граммы
+    weight_grams = weight_in_kg * 1000
+
+    # Конвертация в унции
+    weight_ounces = weight_in_kg * 35.274
+
+    # Конвертация в фунты
+    weight_pounds = weight_in_kg * 2.205
+
+    df = pd.DataFrame({'Единица измерения': ['Граммы', 'Унции', 'Фунты'],
+                       'Значение': [weight_grams, weight_ounces, weight_pounds]})
+    return df
+def convert_weight_grams(weight_in_grams):
+    # Перевод из грамм в килограммы
+    weight_in_kg = weight_in_grams / 1000.0
+
+    # Перевод из грамм в унции (1 грамм = 0.03527396 унции)
+    weight_in_ounces = weight_in_grams * 0.03527396
+
+    # Перевод из грамм в фунты (1 грамм = 0.00220462 фунта)
+    weight_in_pounds = weight_in_grams * 0.00220462
+
+    df = pd.DataFrame({'Единица измерения': ['Килограммы', 'Унции', 'Фунты'],
+                       'Значение': [weight_in_kg, weight_in_ounces, weight_in_pounds]})
+    return df
+def convert_weight_ounces(weight_in_ounces):
+    # Перевод из унций в килограммы
+    weight_in_kg = weight_in_ounces * 0.0283495
+
+    # Перевод   из унций   из граммы
+    weight_in_grams = weight_in_ounces * 28.3495
+
+    # Перевод из унций в фунты
+    weight_in_pounds = weight_in_ounces * 0.0625
+
+    df = pd.DataFrame({'Единица измерения': ['Килограммы', 'Граммы', 'Фунты'],
+                       'Значение': [weight_in_kg, weight_in_grams, weight_in_pounds]})
+    return df
+def convert_weight_pounds(weight_in_pounds):
+    # Перевод из унций в килограммы
+    weight_in_kg = weight_in_pounds * 0.453592
+
+    # Перевод  из унций   из граммы
+    weight_in_grams = weight_in_pounds * 453.592
+
+    # Перевод из унций в фунты
+    weight_in_ounces = weight_in_pounds * 16
+
+    df = pd.DataFrame({'Единица измерения': ['Килограммы', 'Граммы', 'Унции'],
+                       'Значение': [weight_in_kg, weight_in_grams, weight_in_ounces]})
+    return df
---
+++ main.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import covector as cvt
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
+def show_result(df):
+    c1, c2, c3 = st.columns(3)
+    with c2:
+        st.dataframe(df, hide_index = True, width = 450)
+local_css("style.css")
+
+if 'unit' not in st.session_state:
+    st.session_state['unit'] = 'none'
+
+st.title("Конвектор единиц веса")
+st.write("Единица измерения исходного числа")
+
+kg, gr, lb, oz = st.columns(4)
+with kg:
+    kg_btn = st.button("Кг")
+    if kg_btn:
+        st.session_state.unit = 'kg'
+with gr:
+    gr_btn = st.button("Гр")
+    if gr_btn:
+        st.session_state.unit = 'gr'
+with lb:
+    lb_btn = st.button("Lb")
+    if lb_btn:
+        st.session_state.unit = 'lb'
+with oz:
+    oz_btn = st.button("Oz")
+    if oz_btn:
+        st.session_state.unit = 'oz'
+
+value = st.number_input("Введеите значение ", 0)
+
+btn = st.button("Перевести",type="primary")
+if btn:
+    match st.session_state.unit:
+        case 'kg':
+            st.write(f"{value} кг -")
+            show_result(cvt.convert_weight_kg(value))
+        case 'gr':
+            st.write(f"{value} грамм -")
+            show_result(cvt.convert_weight_grams(value))
+        case 'lb':
+            st.write(f"{value} фунтов -")
+            show_result(cvt.convert_weight_pounds(value))
+        case 'oz':
+            st.write(f"{value} унций -")
+            show_result(cvt.convert_weight_ounces(value))
+        case 'none':
+            st.warning("Выберете единицу веса измрения исходного значения", icon="⚠️")
